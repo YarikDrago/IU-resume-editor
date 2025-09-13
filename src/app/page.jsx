18fb2ac501441614c9b4237e99b2df05ad3afd25
Iaroslav Uliantcev <doudou239@gmail.com>
2025-09-13 20:03:06 +0200

    Move metadata definitions from `Meta` component to `page.jsx` for improved modularity and performance.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,22 @@
 import React from 'react';
 import Builder from '../components/builder';
 
+export const metadata = {
+  keywords: [
+    'ATS resume builder',
+    'ATS-friendly resume',
+    'resume builder',
+    'resume editor',
+    'resume maker',
+    'free resume builder',
+    'resume templates',
+    'CV builder',
+    'optimize resume for ATS',
+    'professional resume',
+    'ATSResume',
+  ],
+};
+
 const Page = () => {
   return (
     <div>
