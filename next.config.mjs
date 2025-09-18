92a1f40d3be05df04299b19a96e4c757280e8d04
Iaroslav Uliantcev <doudou239@gmail.com>
2025-09-18 21:22:58 +0200

    Add SVGR webpack configuration and dependency for SVG handling in Next.js

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,34 @@ const nextConfig = {
   images: {
     domains: ['avatars.githubusercontent.com'],
   },
+  webpack(config) {
+    // Turn off the default svg loader
+    config.module.rules = config.module.rules.map((rule) => {
+      if (rule.test?.test?.('.svg')) {
+        return { ...rule, exclude: /\.svg$/i };
+      }
+      return rule;
+    });
+
+    // SVGR settings
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            icon: true,
+            svgo: true,
+            titleProp: true,
+            ref: true,
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
